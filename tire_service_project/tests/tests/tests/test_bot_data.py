0319b3e2ea4a88b35112ed199dcf1a8e1d85090d
ivan16092005-arch <ivan16092005@mail.ru>
2026-06-02 22:32:41 +0300

    Delete tire_service_project directory

--- tire_service_project/tests/tests/tests/test_bot_data.py
+++
@@ -1,6 +0,0 @@
-def test_message_format():
-    message = "Иван;Замена шин;15.06.2026"
-
-    parts = message.split(";")
-
-    assert len(parts) == 3
--- tire_service_project/tests/tests/tests/test_dashboard.py
+++
@@ -1,9 +0,0 @@
-from database import SessionLocal
-from models import Appointment
-
-def test_get_appointments():
-    db = SessionLocal()
-
-    appointments = db.query(Appointment).all()
-
-    assert isinstance(appointments, list)
